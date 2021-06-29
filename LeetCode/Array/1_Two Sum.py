28056fd911b4d4247d5b734e1c9a72e171f269a2
Rahil <rahilmehta18@gmail.com>
2021-06-29 16:53:09 +0530

    first array problem

--- LeetCode/Array/1_Two Sum.py
+++ LeetCode/Array/1_Two Sum.py
@@ -1,8 +1,6 @@
 '''Given an array of integers nums and an integer target, return indices of the two numbers such that they add up to target.
 You may assume that each input would have exactly one solution, and you may not use the same element twice.
-You can return the answer in any order.
-
- 
+You can return the answer in any order. 
 
 Example 1:
 Input: nums = [2,7,11,15], target = 9
@@ -17,6 +15,8 @@ Example 3:
 Input: nums = [3,3], target = 6
 Output: [0,1]'''
 
+ 
+
  # Method 1 (The Brute force way)
 
 Input = [2,7,11,15]
@@ -28,9 +28,10 @@ for i in range(len(Input)):
         if Input[i]+Input[j] == target:
             output = [Input[i],Input[j]]
             
-
 print(output)
 #Time complexity is O(n^2)
+
+
 
 # Method 2 
 
